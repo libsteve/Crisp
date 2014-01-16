7c52a8e8b354e901db51ad0bcdf7b203c1da291f
Steven Brunwasser <sbrunwasser@gmail.com>
2014-01-16 02:40:04 -0500

    renamed mylsip.hpp to cons.hpp

--- example.cpp
+++ example.cpp
@@ -1,22 +1,9 @@
-#include "include/mylisp.hpp"
-#include <boost/variant.hpp>
+#include "include/cons.hpp"
 using namespace mylisp;
 
-typedef boost::variant<std::string, char, bool> data;
-std::ostream& operator << (std::ostream& os, data& d) {
-    struct p : boost::static_visitor<void> {
-        p(std::ostream& s) : os(s) {}
-        std::ostream& os;
-        void operator () (std::string& s) { os << s; }
-        //void operator () (int i) { os << i; }
-        void operator () (char c) { os << c; }
-        void operator () (bool b) { os << (b ? "#t" : "#f"); }
-    } print(os);
-    boost::apply_visitor(print, d);
-    return os;
-}
-
 int main() {
+    std::cout << std::boolalpha;
+
     cons<std::string> f("hello", null);
     cons<std::string> d("world", f);
     cons<std::string> e("goodbye", d);
@@ -30,6 +17,6 @@ int main() {
     std::cout << cons<int>(5, cons<int>(15, 10)) << std::endl;
     std::cout << cons<int>(cons<int>(null, cons<int>(10, cons<int>(15, null))), 5) << std::endl;
 
-    std::cout << cons<data>(std::string("string"), cons<data>('c', cons<data>(bool(false), null))) << std::endl;
+    std::cout << cons<std::string, char, bool>(std::string("string"), cons<std::string, char, bool>('c', cons<std::string, char, bool>(true, null))) << std::endl;
 }
 
